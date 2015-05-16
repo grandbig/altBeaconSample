52cefb8f2492acd2a7299c25b2e48e38fd30caf7
加藤 雄大 <takahiro.kato15@gmail.com>
2015-05-16 22:19:33 +0900

    レンジング処理を追加

--- app/src/main/java/com/example/takahiro/altbeaconsample/MainActivity.java
+++ app/src/main/java/com/example/takahiro/altbeaconsample/MainActivity.java
@@ -3,22 +3,30 @@ package com.example.takahiro.altbeaconsample;
 import android.os.RemoteException;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
 
 
 public class MainActivity extends ActionBarActivity implements BeaconConsumer {
 
+    private static String TAG = "MyApp";
     private BeaconManager beaconManager;
     // iBeaconのデータを認識するためのParserフォーマット
     public static final String IBEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";
+    // UUIDの作成
+    private Identifier identifier = Identifier.parse("434B4666-E4B2-4B78-8C13-901AFEE3DBB6");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,21 +67,48 @@ public class MainActivity extends ActionBarActivity implements BeaconConsumer {
             @Override
             public void didEnterRegion(Region region) {
                 // 領域侵入時に実行
+                Log.d(TAG, "didEnterRegion");
+
+                try {
+                    // レンジングの開始
+                    beaconManager.startRangingBeaconsInRegion(new Region("unique-id-001", identifier, null, null));
+                } catch(RemoteException e) {
+                    // 例外が発生した場合
+                    e.printStackTrace();
+                }
             }
 
             @Override
             public void didExitRegion(Region region) {
                 // 領域退出時に実行
+                Log.d(TAG, "didExitRegion");
+                try {
+                    // レンジング停止
+                    beaconManager.stopRangingBeaconsInRegion(new Region("unique-id-001", identifier, null, null));
+                } catch(RemoteException e) {
+                    // 例外が発生した場合
+                    e.printStackTrace();
+                }
             }
 
             @Override
             public void didDetermineStateForRegion(int i, Region region) {
                 // 領域への侵入/退出のステータスが変化したときに実行
+                Log.d(TAG, "didDetermineStateForRegion");
             }
         });
+
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                // 検出したビーコンの情報を全部Logに書き出す
+                for(Beacon beacon : beacons) {
+                    Log.d(TAG, "UUID:" + beacon.getId1() + ", major:" + beacon.getId2() + ", minor:" + beacon.getId3() + ", Distance:" + beacon.getDistance() + ",RSSI" + beacon.getRssi() + ", TxPower" + beacon.getTxPower());
+                }
+            }
+        });
+
         try {
-            // UUIDの作成
-            Identifier identifier = Identifier.parse("434B4666-E4B2-4B78-8C13-901AFEE3DBB6");
             // モニタリングの開始
             beaconManager.startMonitoringBeaconsInRegion(new Region("unique-id-001", identifier, null, null));
         } catch(RemoteException e) {
